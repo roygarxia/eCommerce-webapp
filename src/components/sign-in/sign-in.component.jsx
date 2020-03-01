9b0216e081482d35f9b27153c8d6e9ded24274fa
Roy Garcia <56310492+roytronics@users.noreply.github.com>
2020-02-29 16:45:03 -0800

    finishing sign in handleSubmit methd

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component.jsx';
 import './sign-in.styles.scss';
 import CustomButton from '../custom-button/custom-button.component.jsx';
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -14,9 +14,21 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''})  
+
+        const { email, password } = this.state;
+
+        try {
+
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});  
+
+        } catch (error) {
+            console.log(error);
+        }
+
+        
     }
 
     handleChange = event => {
