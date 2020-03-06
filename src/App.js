7a7ee59b9d2c1a0cd0f87e16ed585ae85514283d
Roy Garcia <56310492+roytronics@users.noreply.github.com>
2020-03-05 20:52:45 -0800

    deployed app in Heroku and made some CSS changes to make it mobile friendly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import './App.css';
+import {GlobalStyle} from './global.styles';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
@@ -40,6 +40,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle/>
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
---
+++ src/global.styles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
+body {
+    font-family: 'Unica One';
+    padding: 20px 60px;
+
+    @media screen and (max-width: 800px) {
+        padding: 10px;
+        
+    }
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+`;
